3ee767c49286cc06b01069cf8d394fdded8f0470
Tiến Lê <ww.hacker01@gmail.com>
2023-11-19 12:19:44 +0700

    Fix
    Sai tên đường dẫn đến logo, css đăng nhập

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
@@ -1,5 +1,10 @@
 package hcmute.alohcmute.controllers;
 
-public class WebController {
+import org.springframework.web.bind.annotation.GetMapping;
 
+public class WebController {
+	@GetMapping("/")
+	public String home() {
+		return "web/dangnhap/dangnhap.html";
+	}
 }
