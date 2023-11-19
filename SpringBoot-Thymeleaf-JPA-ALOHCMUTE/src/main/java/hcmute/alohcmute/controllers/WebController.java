d0b2820929af84fd5ed2540ac86ee367bc1d7ae6
Tiến Lê <ww.hacker01@gmail.com>
2023-11-19 12:41:20 +0700

    Auto stash before merge of "Review" and "origin/Khang"

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 	@GetMapping("/")
 	public String home() {
-		return "web/dangnhap/dangnhap.html";
+		return "user/banbe/banbe.html";
 	}
 }
