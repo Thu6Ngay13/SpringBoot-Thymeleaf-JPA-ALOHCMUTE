b49b270850accf3b5ae2ecc227f77b71016925be
Khang <khangatvttt@gmail.com>
2023-11-19 13:14:22 +0700

    fix banbe
    fix banbe

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
@@ -1,7 +1,9 @@
 package hcmute.alohcmute.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class WebController {
 	@GetMapping("/")
 	public String home() {
