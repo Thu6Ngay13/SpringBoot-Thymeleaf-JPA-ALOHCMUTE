9c15df7f673253afb3347df118aa121d26290d48
Khanh <nguyennhatkhanh12102019@gmail.com>
2023-11-24 16:11:10 +0700

    Update
    sửa  lại tí trang newfeed

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 	@GetMapping("/")
 	public String home() {
-		return "user/baidang/baidang.html";
+		return "user/newfeed/newfeed.html";
 	}
 	
 }
