464b134b2c8858a76fbcad4525540df41a72bb72
Khanh <nguyennhatkhanh12102019@gmail.com>
2023-11-24 16:29:08 +0700

    Update
    Sửa tí trang banbe

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 	@GetMapping("/")
 	public String home() {
-		return "user/newfeed/newfeed.html";
+		return "user/banbe/banbe.html";
 	}
 	
 }
