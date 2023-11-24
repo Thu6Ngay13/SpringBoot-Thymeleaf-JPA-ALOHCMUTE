47ebd152776a3d3561e381e33232f555e5811b0a
quynhhtm <manhquynh458@gmail.com>
2023-11-24 09:00:21 +0700

    Update tạm thời

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 	@GetMapping("/")
 	public String home() {
-		return "web/dangky/dangky.html";
+		return "admin/manage/quanlybaiviet.html";
 	}
 }
