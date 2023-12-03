a774ebff3cb139bb54681561b57e652a59ed454e
CAO816 <caothithuthuy00000@gmail.com>
2023-12-03 09:46:07 +0700

    đừng merge vô chung nha
    đưa bình luận lên trang comment

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
@@ -1,14 +1,24 @@
 package hcmute.alohcmute.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import hcmute.alohcmute.entities.BinhLuan;
+import hcmute.alohcmute.services.CommentSerrviceImpl;
+import hcmute.alohcmute.services.ICommentService;
+
 @Controller
 public class CommentController {
-	
-	@GetMapping("/")
+	@Autowired
+	ICommentService commentService = new CommentSerrviceImpl();
+	@GetMapping("/user")
 	public String reviewComment(ModelMap model) {
+		List<BinhLuan> comments = commentService.findCommentByMaBaiViet(1);
+		model.addAttribute("comments", comments);
 		return "user/comment/comment";
 	}
 }
