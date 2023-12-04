6eae92fba5c9e970a98b36f7d4885e52c4431960
CAO816 <caothithuthuy00000@gmail.com>
2023-12-04 13:30:22 +0700

    update Comment

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import hcmute.alohcmute.entities.BinhLuan;
 import hcmute.alohcmute.services.CommentSerrviceImpl;
 import hcmute.alohcmute.services.ICommentService;
 
 @Controller
+@RequestMapping("user")
 public class CommentController {
 	@Autowired
 	ICommentService commentService = new CommentSerrviceImpl();
