9e9d809786259b001559ec95b327430c2d028575
CAO816 <caothithuthuy00000@gmail.com>
2023-12-04 08:55:21 +0700

    Auto stash before merge of "Thuy" and "origin/Huyen"

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
@@ -2,10 +2,12 @@ package hcmute.alohcmute.controllers;
 
 import java.util.List;
 
+import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import hcmute.alohcmute.entities.BinhLuan;
 import hcmute.alohcmute.services.CommentSerrviceImpl;
@@ -15,9 +17,9 @@ import hcmute.alohcmute.services.ICommentService;
 public class CommentController {
 	@Autowired
 	ICommentService commentService = new CommentSerrviceImpl();
-	@GetMapping("/user")
-	public String reviewComment(ModelMap model) {
-		List<BinhLuan> comments = commentService.findCommentByMaBaiViet(1);
+	@GetMapping("/comment/{baiVietId}")
+	public String reviewComment(ModelMap model, @PathVariable(value="baiVietId") int id) {
+		List<BinhLuan> comments = commentService.findCommentByMaBaiViet(id);
 		model.addAttribute("comments", comments);
 		return "user/comment/comment";
 	}
