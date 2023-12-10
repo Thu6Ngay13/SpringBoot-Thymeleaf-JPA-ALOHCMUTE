2d20a3ec90e0dc2832a191c953632d5f2d1b9cae
Tiến Lê <ww.hacker01@gmail.com>
2023-12-10 16:28:08 +0700

    Fix
    + Đưa các method REST từ controller user và sang controller admin

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/UserController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/UserController.java
@@ -1,52 +1,16 @@
 package hcmute.alohcmute.controllers;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import hcmute.alohcmute.entities.BaoCaoBaiViet;
-import hcmute.alohcmute.entities.TaiKhoan;
 import hcmute.alohcmute.services.ITaiKhoanService;
 
 @Controller
 public class UserController {
 	@Autowired
 	ITaiKhoanService taiKhoanService;
-	
-	@GetMapping(value = {"/admin/chitiet/banUser/{taikhoan}/{mabaocao}", "/admin/chitiet/unbanUser/{taikhoan}/{mabaocao}"})
-	public String banUser(@PathVariable(value = "taikhoan") String taikhoan, @PathVariable(value = "mabaocao") int mabaocao) {
-		Optional<TaiKhoan> Opttaikhoan = taiKhoanService.findById(taikhoan); 
-		TaiKhoan userban = Opttaikhoan.get();
-		if (userban.isEnable()== true) {
-			userban.setEnable(false);
-		}
-		else {
-			userban.setEnable(true);
-		}
-		taiKhoanService.save(userban);
-		
-		return "redirect:/admin/chitiet/{mabaocao}";
-	}
-	
-	public String AdminbanUser(@PathVariable(value = "taikhoan") String taikhoan) {
-		Optional<TaiKhoan> Opttaikhoan = taiKhoanService.findById(taikhoan); 
-		TaiKhoan userban = Opttaikhoan.get();
-		if (userban.isEnable()== true) {
-			userban.setEnable(false);
-		}
-		else {
-			userban.setEnable(true);
-		}
-		taiKhoanService.save(userban);
-		
-		return "redirect:/admin/manage/thongtintaikhoan/{taikhoan}";
-	}
-	
 	
 	@GetMapping("/user")
 	public String userPage(Model model) {
