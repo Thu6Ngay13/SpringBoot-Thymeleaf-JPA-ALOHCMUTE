70297950f75454058ae3e2f0f7eae7d2558abec0
CAO816 <caothithuthuy00000@gmail.com>
2023-12-11 10:43:36 +0700

    chức năng tìm kiếm
    backend chức năng tìm kiếm

---
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/SearchController.java
@@ -0,0 +1,33 @@
+package hcmute.alohcmute.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import hcmute.alohcmute.models.TaiKhoanModel;
+import hcmute.alohcmute.services.ITaiKhoanService;
+
+@Controller
+public class SearchController {
+	@Autowired
+	ITaiKhoanService taiKhoanService;
+	String username = "lolo928";
+	@GetMapping("/user/search/{keywork}")
+	public String home(ModelMap model, @PathVariable(value = "keywork") String keywork) {
+		List<TaiKhoanModel> listTaiKhoan = taiKhoanService.findTaiKhoanByKeyword(keywork, username);
+		List<TaiKhoanModel> list = new ArrayList<>();
+		if (listTaiKhoan.size() != 0) {
+			for (TaiKhoanModel tk : listTaiKhoan) {
+				if (tk.getTaiKhoan().equals(username) == false || tk.getTaiKhoan().equals("admin") == false)
+					list.add(tk);
+			}
+		}
+		model.addAttribute("listTaiKhoan", list);
+		return "user/search/search";
+	}
+}
--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-	@GetMapping("/user/search")
+	@GetMapping("/user")
 	public String home(ModelMap model) {
 		return "user/search/search";
 	}
