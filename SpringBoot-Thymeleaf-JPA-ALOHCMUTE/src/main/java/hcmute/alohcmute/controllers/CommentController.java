e80081e9c8f08a327c8017e44caa5b1aeef6f440
thanhhuyendc <102838052+thanhhuyendc@users.noreply.github.com>
2023-12-11 13:06:52 +0700

    Update
     tag tài khoản trong comment, hộp suggestion di chuyển theo @, lưu thông báo

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/controllers/CommentController.java
@@ -3,7 +3,8 @@ package hcmute.alohcmute.controllers;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,16 +14,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hcmute.alohcmute.entities.BaiViet;
 import hcmute.alohcmute.entities.BinhLuan;
 import hcmute.alohcmute.entities.TaiKhoan;
+import hcmute.alohcmute.entities.ThongBao;
 import hcmute.alohcmute.services.BaiVietServiceImpl;
 import hcmute.alohcmute.services.CommentSerrviceImpl;
 import hcmute.alohcmute.services.IBaiVietService;
 import hcmute.alohcmute.services.ICommentService;
 import hcmute.alohcmute.services.ITaiKhoanService;
+import hcmute.alohcmute.services.IThongBaoService;
 import hcmute.alohcmute.services.TaiKhoanServiceImpl;
+import hcmute.alohcmute.services.ThongBaoServiceImpl;
 import jakarta.validation.Valid;
 
 @Controller
@@ -34,6 +39,9 @@ public class CommentController {
     IBaiVietService baiVietService = new BaiVietServiceImpl();
 	@Autowired
     ITaiKhoanService taiKhoanService = new TaiKhoanServiceImpl();
+	
+	@Autowired
+	IThongBaoService thongBaoService = new ThongBaoServiceImpl();
 
 	@GetMapping("/comment/{baiVietId}")
 	public String reviewComment(ModelMap model, @PathVariable(value = "baiVietId") int id) {
@@ -73,6 +81,29 @@ public class CommentController {
 			binhLuan.setThoiGian(java.time.LocalTime.now().truncatedTo(ChronoUnit.MINUTES));
 			binhLuan.setBaiViet(baiVietService.getById(id));
 			binhLuan.setTaiKhoan(taiKhoanService.findBytaiKhoan("thuycao816"));
+			
+			TaiKhoan taikhoan=taiKhoanService.findBytaiKhoan("thuycao816");
+			
+			
+			Pattern pattern = Pattern.compile("@\\w+");
+
+	        // Create a matcher for the input string
+	        Matcher matcher = pattern.matcher(binhLuan.getNoiDungChu());
+
+	        // Find and print all occurrences
+	        while (matcher.find()) {
+	            String match = matcher.group();
+	            ThongBao tb = new ThongBao();
+	            tb.setNgay(java.time.LocalDate.now());
+	            String NoiDung = taikhoan.getHoTen()+" đã nhắc đến bạn trong một bình";
+	            tb.setNoiDung(NoiDung);
+	            String user=match.substring(1);
+	            tb.setTaiKhoan(taiKhoanService.findBytaiKhoan(user));
+	            tb.setThoiGian(java.time.LocalTime.now());
+	            thongBaoService.save(tb);
+	        }
+
+			
 			commentService.save(binhLuan);
 		}
 		return "redirect:{baiVietId}";
