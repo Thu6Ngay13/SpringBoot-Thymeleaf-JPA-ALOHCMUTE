fd78cb66a6173edeaa434f869324ec4ff1d2c713
trngh <nghiale0504@gmail.com>
2023-12-12 15:32:42 +0700

    Update
    chuyển về /user/newfeed

--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/repositories/BaiVietRepository.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/repositories/BaiVietRepository.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import hcmute.alohcmute.entities.BaiViet;
 import hcmute.alohcmute.entities.Nhom;
-import jakarta.transaction.Transactional;
 
 @Repository
 public interface BaiVietRepository extends JpaRepository<BaiViet, Integer>{
--- SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/repositories/ThongBaoRepository.java
+++ SpringBoot-Thymeleaf-JPA-ALOHCMUTE/src/main/java/hcmute/alohcmute/repositories/ThongBaoRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 import hcmute.alohcmute.entities.TaiKhoan;
 import hcmute.alohcmute.entities.ThongBao;
 @Repository
-public interface ThongBaoRepository extends JpaRepository<ThongBao, String> {
+public interface ThongBaoRepository extends JpaRepository<ThongBao, Integer> {
 	List<ThongBao> findByTaiKhoan(TaiKhoan taiKhoan);
 }
